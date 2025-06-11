ce0abab54c5a06cc935f6cfec7f5bbdc60ea08ef
jangk34 <41567419+jangk34@users.noreply.github.com>
2025-06-11 16:00:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+const port = process.env.PORT || 3000;
+const http = require('http');
+const fs = require('fs');
 
-var log = function(entry) {
+const html = fs.readFileSync('index.html');
+
+const log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
 
-var server = http.createServer(function (req, res) {
+const server = http.createServer((req, res) => {
     if (req.method === 'POST') {
-        var body = '';
+        let body = '';
 
-        req.on('data', function(chunk) {
+        req.on('data', chunk => {
             body += chunk;
         });
 
-        req.on('end', function() {
+        req.on('end', () => {
             if (req.url === '/') {
                 log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
+            } else if (req.url === '/scheduled') {
                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
             }
 
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('OK');
         });
     } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(html);
     }
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+server.listen(port, () => {
+    console.log(`Server running at http://127.0.0.1:${port}/`);
+});
